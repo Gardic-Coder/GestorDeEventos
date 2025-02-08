3564f2f6bbda31abb916febd04744966dd3035b1
Gardic-Coder <elsujetojg@gmail.com>
2025-02-08 01:32:21 -0400

    Ver 0.0

---
+++ GestorDeEventos/src/main/java/com/organizacion/domain/exceptions/EventoNoEncontradoException.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package main.java.com.organizacion.domain.exceptions;
+
+/**
+ *
+ * @author juanm
+ */
+public class EventoNoEncontradoException extends Exception {
+
+    /**
+     * Creates a new instance of <code>EventoNoEncontradoException</code>
+     * without detail message.
+     */
+    public EventoNoEncontradoException() {
+    }
+
+    /**
+     * Constructs an instance of <code>EventoNoEncontradoException</code> with
+     * the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public EventoNoEncontradoException(String msg) {
+        super(msg);
+    }
+}
