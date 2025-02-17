0b2be47e690e9a6e74cdcaa60c87537518b74af4
YasaiDaikon <algonzalez90@hotmail.com>
2025-02-16 21:41:01 -0400

    Ver. 0.0.1
    Cimientos de la carpeta de funciones de CRUD (Create, Read, Update and Delete)

--- GestorDeEventos/src/funcionesGestionEventos/ParticipanteService.java
+++ GestorDeEventos/src/funcionesGestionEventos/ParticipanteService.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main.java.com.organizacion.application.services;
+package funcionesGestionEventos;
 
 /**
  *
  * @author juanm
  */
-public class EventoService {
+public class ParticipanteService {
     
 }
