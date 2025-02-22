230ead0f5559f11f6e60e3d9ba989485a8614a5a
Gardic-Coder <elsujetojg@gmail.com>
2025-02-22 18:08:49 -0400

    Ver 0.2.1
    Servicio

--- GestorDeEventos/src/main/java/application/services/ParticipanteService.java
+++ GestorDeEventos/src/main/java/application/services/ParticipanteService.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main.java.application.services;
 
-/**
- *
- * @author juanm
- */
+import java.util.ArrayList;
+import java.util.List;
+import main.java.application.dto.*;
+import main.java.domain.*;
+
+
 public class ParticipanteService {
+    EventoService eventoService = new EventoService();
     
+    public void agregarParticipante(ParticipanteDTO participante) {
+       List<EventoDTO> eventosDTO = eventoService.listaDeEventoDTO();
+       List<Evento> eventos = new ArrayList<>();
+       Participante nuevoParticipante = ParticipanteMapper.fromDTO(participante);
+       
+       eventosDTO.forEach(eventoDTO -> {
+           eventos.add(EventoMapper.fromDTO(eventoDTO));
+        });
+       
+       eventos.stream().filter(evento -> (evento.getID().equals(participante.getEvento()))).forEachOrdered(evento -> {
+           evento.agregarParticipante(nuevoParticipante);
+        });
+       
+       eventoService.guardarTodo(eventos);
+    }
+    
+    public void eliminarParticipante(ParticipanteDTO participante) {
+       List<EventoDTO> eventosDTO = eventoService.listaDeEventoDTO();
+       List<Evento> eventos = new ArrayList<>();
+       Participante participanteEliminar = ParticipanteMapper.fromDTO(participante);
+       
+       eventosDTO.forEach(eventoDTO -> {
+           eventos.add(EventoMapper.fromDTO(eventoDTO));
+        });
+       
+       eventos.stream().filter(evento -> (evento.getID().equals(participanteEliminar.getEvento()))).forEachOrdered((Evento evento) -> {
+           evento.eliminarParticipante(participanteEliminar);
+        });
+       
+       eventoService.guardarTodo(eventos);
+    }
 }
